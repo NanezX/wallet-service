b1a489f92cc18f4bc85aaec290e32bb51197c0dd
NanezX <victor.nanezj@gmail.com>
2026-05-23 13:49:55 -0400

    nestjs setup

---
+++ jest.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from 'jest';
+
+const commonConfig = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+};
+
+const config: Config = {
+  projects: [
+    {
+      ...commonConfig,
+      displayName: 'unit',
+      rootDir: '.',
+      testMatch: ['<rootDir>/src/**/*.spec.ts'],
+    },
+    {
+      ...commonConfig,
+      displayName: 'integration',
+      rootDir: '.',
+      testMatch: ['<rootDir>/test/integration/**/*.spec.ts'],
+    },
+  ],
+};
+
+export default config;
